e21cb3ec18429ac4e327c6f498b105bde84186a3
Александр Буланов <alex-bulanov.mail@yandex.ru>
2019-05-09 13:36:24 +0300

    Защита 1-я попытка

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,7 +1,48 @@
-var isStorageSupport = true;
-var storage = "";
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
+
+document.addEventListener('DOMContentLoaded', function() {
+  var form = document.querySelector('.form__page-form');
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+  });
+  var name = form.querySelector('[name=name]');
+  var email = form.querySelector('[name=email]');
+  var weight = form.querySelector('[name=weight]');
+  var phone = form.querySelector('[name=phone]');
+
+  name.addEventListener('change', function(e) {
+    if (name.validity.typeMismatch || (name.value == false)) {
+      name.classList.add('form__input--invalid');
+    } else {
+      name.classList.remove('form__input--invalid');
+    }
+  });
+
+  weight.addEventListener('change', function(e) {
+    if (weight.value == false || weight.validity.patternMismatch) {
+      weight.classList.add('form__input--invalid');
+    } else {
+      weight.classList.remove('form__input--invalid');
+    }
+  });
+
+  email.addEventListener('change', function(e) {
+    if (email.value == false || email.validity.typeMismatch || email.validity.patternMismatch) {
+      email.classList.add('form__input--invalid');
+    } else {
+      email.classList.remove('form__input--invalid');
+    }
+  });
+
+  phone.addEventListener('change', function(e) {
+    if (phone.value == false || phone.validity.typeMismatch || phone.validity.patternMismatch) {
+      phone.classList.add('form__input--invalid');
+    } else {
+      phone.classList.remove('form__input--invalid');
+    }
+  });
+})
 
 navMain.classList.remove('main-nav--nojs');
 navToggle.addEventListener('click', function() {
